ee66059340d80e15857366bdc0c945ed9127d9ca
damnkk <17611288725@163.com>
2023-01-05 17:55:09 +0800

    step

--- Vulkan.cpp
+++ Vulkan.cpp
@@ -8,6 +8,8 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
 
 #include <iostream>
 #include <fstream>
@@ -251,6 +253,7 @@ private:
     createDescriptorSets();
     createCommandBuffers();
     createSyncObjects();
+    loadObjModle();
   }
 
   void mainLoop()
@@ -1685,6 +1688,59 @@ private:
   bool hasStencilComponent(VkFormat format)
   {
     return format == VK_FORMAT_D32_SFLOAT_S8_UINT || VK_FORMAT_D24_UNORM_S8_UINT;
+  }
+  void loadObjModle(){
+
+    std::string modelPath = "C:/Users/admin/JYF_Repositories/Vulkan_learn/models/nanosuit/nanosuit.obj";
+    tinyobj::ObjReaderConfig reader_config;
+    reader_config.mtl_search_path = "C:/Users/admin/JYF_Repositories/Vulkan_learn/models/nanosuit";
+    tinyobj::ObjReader reader;
+
+    if (!reader.ParseFromFile(modelPath, reader_config)) {
+
+  exit(1);
+}
+
+if (!reader.Warning().empty()) {
+  std::cout << "TinyObjReader: " << reader.Warning();
+}
+
+    auto &attrib = reader.GetAttrib();
+    auto &shapes = reader.GetShapes();
+    auto &material = reader.GetMaterials();
+    for (size_t s = 0; s < shapes.size();s++){
+      size_t index_offset = 0;
+      for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size();++f){
+        size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
+        for (size_t v = 0; v < fv;++v){
+              // access to vertex
+          tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+          tinyobj::real_t vx = attrib.vertices[3*size_t(idx.vertex_index)+0];
+          tinyobj::real_t vy = attrib.vertices[3*size_t(idx.vertex_index)+1];
+          tinyobj::real_t vz = attrib.vertices[3*size_t(idx.vertex_index)+2];
+
+          // Check if `normal_index` is zero or positive. negative = no normal data
+          if (idx.normal_index >= 0) {
+            tinyobj::real_t nx = attrib.normals[3*size_t(idx.normal_index)+0];
+            tinyobj::real_t ny = attrib.normals[3*size_t(idx.normal_index)+1];
+            tinyobj::real_t nz = attrib.normals[3*size_t(idx.normal_index)+2];
+          }
+
+          // Check if `texcoord_index` is zero or positive. negative = no texcoord data
+          if (idx.texcoord_index >= 0) {
+            tinyobj::real_t tx = attrib.texcoords[2*size_t(idx.texcoord_index)+0];
+            tinyobj::real_t ty = attrib.texcoords[2*size_t(idx.texcoord_index)+1];
+          }
+
+          // Optional: vertex colors
+          // tinyobj::real_t red   = attrib.colors[3*size_t(idx.vertex_index)+0];
+          // tinyobj::real_t green = attrib.colors[3*size_t(idx.vertex_index)+1];
+          // tinyobj::real_t blue  = attrib.colors[3*size_t(idx.vertex_index)+2];
+        }
+        index_offset += fv;
+      }
+      std::cout << material[shapes[s].mesh.material_ids[0]].diffuse_texname << std::endl;
+    }
   }
 
   static std::vector<char> readFile(const std::string &filename)
